039884a977b2d94a8eb530d32e8c2919f79145b6
Dalai Aguirre <49468436+dalai2@users.noreply.github.com>
2024-07-24 20:02:53 -0600

    Add files via upload

--- send_to_telegram.py
+++ send_to_telegram.py
@@ -34,7 +34,7 @@ def init_driver():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument("--user-data-dir=/Users/YourUsername/Library/Application Support/Google/Chrome")
+    chrome_options.add_argument("--user-data-dir=/Users/sandrajimenez/Library/Application Support/Google/Chrome")
     chrome_options.add_argument('--profile-directory=Default')
     chrome_options.add_argument("--start-maximized")
 
