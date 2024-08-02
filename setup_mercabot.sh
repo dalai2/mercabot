7be4874d9854fc865fe95f4b6be3e320b6896abb
Dalai Aguirre <49468436+dalai2@users.noreply.github.com>
2024-08-02 11:23:50 -0600

    Add files via upload

--- setup_mercabot.sh
+++ setup_mercabot.sh
@@ -1,5 +1,22 @@
+#!/bin/bash
+
 # Define the path for the plist file
-PLIST_FILE=~/Library/LaunchAgents/com.sandrajimenez.mercabot.plist
+PLIST_FILE=~/Library/LaunchAgents/com.isaac.mercabot.plist
+
+# Create a wrapper script to activate the virtual environment and run the Python script
+WRAPPER_SCRIPT=~/Desktop/run_mercabot.sh
+cat << 'EOF' > $WRAPPER_SCRIPT
+#!/bin/bash
+
+# Activate the virtual environment
+source ~/Desktop/mercabot/venv/bin/activate
+
+# Run the Python script
+/usr/bin/python3 /Users/isaac/Desktop/mercabot/send_to_telegram_2.py
+EOF
+
+# Make the wrapper script executable
+chmod +x $WRAPPER_SCRIPT
 
 # Create the plist content
 cat << EOF > $PLIST_FILE
@@ -8,25 +25,25 @@ cat << EOF > $PLIST_FILE
 <plist version="1.0">
 <dict>
     <key>Label</key>
-    <string>com.sandrajimenez.mercabot</string>
+    <string>com.isaac.mercabot</string>
     <key>ProgramArguments</key>
     <array>
         <string>/bin/bash</string>
-        <string>/Users/isaac/Desktop/mercabot/run_mercabot.sh</string>
+        <string>$WRAPPER_SCRIPT</string>
     </array>
     <key>StartCalendarInterval</key>
     <dict>
         <key>Hour</key>
-        <integer>11</integer>
+        <integer>9</integer>
         <key>Minute</key>
         <integer>0</integer>
     </dict>
     <key>RunAtLoad</key>
     <true/>
     <key>StandardOutPath</key>
-    <string>/tmp/com.sandrajimenez.mercabot.out</string>
+    <string>/tmp/com.isaac.mercabot.out</string>
     <key>StandardErrorPath</key>
-    <string>/tmp/com.sandrajimenez.mercabot.err</string>
+    <string>/tmp/com.isaac.mercabot.err</string>
 </dict>
 </plist>
 EOF
