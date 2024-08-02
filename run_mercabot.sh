0e7694b4c60fcde2ff0a8435cbccba8f092841da
Dalai Aguirre <49468436+dalai2@users.noreply.github.com>
2024-08-02 11:01:22 -0600

    Add files via upload

---
+++ run_mercabot.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source ~/Desktop/mercabot/venv/bin/activate
+
+# Run the Python script
+/usr/bin/python3 /Users/isaac/Desktop/mercabot/send_to_telegram_2.py
